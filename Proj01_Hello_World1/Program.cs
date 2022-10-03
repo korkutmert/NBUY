1536f4b42849a19a2b81be7131d8c01e8dbb5e6b
mert <mertmuslu99@gmail.com>
2022-10-03 18:17:24 +0300

    ilk uygulama 03.10.2022

---
+++ Proj01_Hello_World1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proj01_Hello_World1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
