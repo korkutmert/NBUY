b8830ddff77c202c24cb9aabf63456b48098e614
mert <mertmuslu99@gmail.com>
2022-10-27 18:12:47 +0300

    --

---
+++ Hafta_04/02_Javascript/12_DOM/01-Intro/app.js
@@ -0,0 +1,5 @@
+let sonuc;
+// sonuc= document.getElementsByTagName('div');
+// sonuc=document.getElementsByClassName('card-header');
+sonuc =document.getElementById('task-list')// her zaman yukardan aşağı bakar ilk karşılaştoğını yazar.
+console.log(sonuc);
