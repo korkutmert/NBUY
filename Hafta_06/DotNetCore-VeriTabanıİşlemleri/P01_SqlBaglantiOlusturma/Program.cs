e8bf644258de3da15ea6fb2873b73f3b235ce22c
mert <mertmuslu99@gmail.com>
2022-11-09 18:10:45 +0300

    9.11

--- Hafta_06/DotNetCore-VeriTabanıİşlemleri/P01_SqlBaglantiOlusturma/Program.cs
+++ Hafta_06/DotNetCore-VeriTabanıİşlemleri/P01_SqlBaglantiOlusturma/Program.cs
@@ -1,36 +1,38 @@
-﻿
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 
 namespace P01_SqlBaglantiOlusturma;
+
 class Program
 {
     static void Main(string[] args)
     {
         GetSqlConnection();
+        
     }
     static void GetSqlConnection()
     {
-        //1. Adım --> Connection string olulturmak
-        string connectionString = "Server=DESKTOP-OFVK2FD; Database=Northwind; User Id =sa; Password=123";
-        using (var connection =new SqlConnection(connectionString) )
-        {
-            // coonection nesnesi sadece bu scope içinde yaşayıp, scope bitisinde bellekten kaldırılmış olacak!
-        try
-        {
-            connection.Open();
-            Console.WriteLine("Bağlantı sağlandı..");
+        /* 1.Adım
+            ConnectionString oluşturmak
+        */
+        string connectionString = @"Server=DESKTOP-OFVK2FD\SQLEXPRESS;Database=Northwind; User Id=sa; Password=123";
 
-        }
-        catch (Exception e)
-        {
-            
-            Console.WriteLine(e.Message);
 
-        }
-        finally
+        using (var connection = new SqlConnection(connectionString))
         {
-            connection.Close();
-        }
+            //connection nesnesi sadece bu scope içinde yaşayıp, scope bitişinde bellekten kaldırılmış olacak!
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Bağlantı sağlandı...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);                
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
