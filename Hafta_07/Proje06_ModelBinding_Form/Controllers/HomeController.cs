cf786db74db09fed5c8838d5fd40e859a6dd179a
mert <mertmuslu99@gmail.com>
2022-11-18 18:16:51 +0300

    --

---
+++ Hafta_07/Proje06_ModelBinding_Form/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proje06_ModelBinding_Form.Models;
+
+namespace Proje06_ModelBinding_Form.Controllers;
+
+public class HomeController : Controller
+{
+   
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+     public IActionResult YasGrubu()
+    {
+        return View();
+    }
+    [HttpPost]// Bir alttaki yas grubu action'u post türünde olduğu için hemen üstüne tanumlanır.
+         public IActionResult YasGrubu(int yas, string ad)
+    {   if(yas == 0 )
+        {
+            ViewBag.YasGrubu="Lütfen Yaşınızı Giriniz.";
+        }
+        else if (yas<18)
+        {
+            ViewBag.YasGrubu=$"{ad},Reşit Değilsiniz.";
+        }
+        else if (yas<40)
+        {
+            ViewBag.YasGrubu=$"{ad}, Gençsiniz.";
+        }
+        else if (yas<60)
+        {
+            ViewBag.YasGrubu=$"{ad},Yaşlısınız.";
+        }
+        else
+        {
+            ViewBag.YasGrubu=$"{ad}, Çok Yaşlısınız.";
+
+        }
+        return View();
+    }
+    
+    
+       public IActionResult CreateProduct()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+           public IActionResult CreateProduct(Product product)
+    {
+
+        // Burada veri tabanına kayıt işlemi vb. yapılacak.
+        // Şimdilik biz gelen verilerin testini yapabilmek için tekrar sayafaya bastıralım.
+        ViewBag.ResultHeader=$"{product.Name} adlı ürün eklendi." ;
+        ViewBag.ResultBody=$" Ürün Kategorisi: {product.Category} Fiyat :{product.Price} " ;
+        
+        
+        return View(product);
+    }
+    
+   
+    
+    // public IActionResult VerileriAl(string txtAd ,int txtYas)
+    // {
+    //     return View();
+    // }
+
+}
---
+++ Hafta_07/Proje07_KitabeviApp-mert notlar/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using KitabeviApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KitabeviApp.Controllers;
+
+public class HomeController : Controller
+{
+    KitabeviContext context = new KitabeviContext();
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult KategoriListesi()
+    {
+        var kategoriler = context.Kategoriler.ToList();
+        return View(kategoriler);
+    }
+    public IActionResult YazarListesi()
+    {
+        var yazarlar = context.Yazarlar.ToList();
+        return View(yazarlar);
+    }
+    public IActionResult KitapListesi(int? id = null)
+    {
+        List<Kitap> kitaplar = null;
+        if (id == null)
+        {
+            kitaplar = context
+                .Kitaplar
+                .Include(k => k.Kategori)
+                .Include(k => k.Yazar)
+                .ToList();
+        }
+        else
+        {
+            kitaplar = context
+                .Kitaplar
+                .Where(c => c.KategoriId == id)
+                .Include(k => k.Kategori)
+                .Include(k => k.Yazar)
+                .ToList();
+        }
+        return View(kitaplar);
+    }
+    public IActionResult Detay(int id)
+    {
+        var kitap = context
+            .Kitaplar
+            .Where(k => k.Id == id)
+            .Include(k => k.Yazar)
+            .Include(k => k.Kategori)
+            .FirstOrDefault();
+        return View(kitap);
+    }
+    public IActionResult Get(int id)
+    {
+
+        return View();
+    }
+
+    public IActionResult KategoriEkle()
+    {
+
+        return View();
+    }
+    [HttpPost]
+     public IActionResult KategoriEkle(Kategori kategori)
+    {
+        context.Kategoriler.Add(kategori);// En baştaki kategori tablosuna ekledik
+        context.SaveChanges(); // Veri tabanına kaydetti.
+        return RedirectToAction("KategoriListesi");//KAtegori Listesi action'a geri yolladı. Yani kayıttan sonra katogeri listesi gözükecek.
+    }
+
+    public IActionResult YazarEkle()
+    {
+        return View();
+    }
+}
+
