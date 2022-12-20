bfbd855638b8d1136ad08e3bc35efcfb4e8e0b71
mert <mertmuslu99@gmail.com>
2022-12-20 18:10:25 +0300

    20-12
    Users İşlemleri

---
+++ Hafta_12/20-12-2022 - Users/ShoppingApp/ShoppingApp.Data/Abstract/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using ShoppingApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Abstract
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        //Producta özgü memberlar burada olacak.(property, field, method...)
+        //Örneğin aşağıdakiler gibi:
+        Task<List<Product>> GetProductsByCategoryAsync(string category);
+        Task<List<Product>> GetHomePageProductsAsync();
+        Task<Product> GetProductDetailsByUrlAsync(string productUrl);
+        Task<List<Product>> GetProductsWithCategories(); //Kategorileriyle birlikte PRODUCTLARI getir.
+        Task CreateProductAsync(Product product, int[] selectedCategoryIds);
+        Task<Product> GetProductWithCategories(int id); //Kategorileriyle birlikte PRODUCT'ı getir.
+        Task UpdateProductAsync(Product product, int[] selectedCategoryIds);
+        Task UpdateIsHomeAsync(Product product);
+        Task UpdateIsApprovedAsync(Product product);
+    }
+}
---
+++ Hafta_12/20-12-2022 - Users/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Core;
+using ShoppingApp.Entity.Concrete.Identity;
+using ShoppingApp.Web.Areas.Admin.Models.Dtos;
+using ShoppingApp.Web.EmailServices.Abstract;
+using System.Data;
+using System.Net.Http.Headers;
+
+namespace ShoppingApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+        private readonly IEmailSender _emailSender;
+
+
+        public UserController(UserManager<User> userManager, RoleManager<Role> roleManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            List<UserDto> users = _userManager.Users.Select(u => new UserDto
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                UserName = u.UserName,
+                Email = u.Email,
+                Emailconfirmed = u.EmailConfirmed
+
+            }).ToList();
+            ViewBag.SelectedMenu = "User";
+            ViewBag.Title = "Kullanıcılar";
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            UserAddDto userAddDto = new UserAddDto
+            {
+                Roles = _roleManager.Roles.Select(r => new RoleDto
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    Description = r.Description
+                }).ToList(),
+
+                SelectedRoles = new List<string>()
+            };
+            return View(userAddDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserAddDto userAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+
+                    FirstName = userAddDto.UserDto.FirstName,
+                    LastName = userAddDto.UserDto.LastName,
+                    UserName = userAddDto.UserDto.UserName,
+                    Email = userAddDto.UserDto.Email,
+                    EmailConfirmed = userAddDto.UserDto.Emailconfirmed,
+
+                };
+                var result = await _userManager.CreateAsync(user, "Qwe123.");
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRolesAsync(user, userAddDto.SelectedRoles);
+                    TempData["Message"] = Jobs.CreateMessage("Başarılı", $"{user.UserName} kullanıcısı başarıyla oluşturuldu.", "success");
+                    return RedirectToAction("Index", "User");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            userAddDto.Roles = _roleManager.Roles.Select(r => new RoleDto
+            {
+                Id = r.Id,
+                Name = r.Name,
+                Description = r.Description
+            }).ToList();
+            userAddDto.SelectedRoles = userAddDto.SelectedRoles ?? new List<string>();
+            return View(userAddDto);
+        
+        
+        }     
+    
+        public async Task<IActionResult> Edit (string id)
+        {
+            var user=await _userManager.FindByIdAsync(id);
+            if (user == null) { return NotFound(); }
+            UserAddDto userUpdateDto = new UserAddDto
+            {
+                UserDto = new UserDto
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Emailconfirmed = user.EmailConfirmed,
+                    UserName = user.UserName
+
+                },
+                SelectedRoles = await _userManager.GetRolesAsync(user),
+                Roles = _roleManager.Roles.Select(r => new RoleDto
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    Description = r.Description
+                }).ToList()
+        };
+            return View(userUpdateDto);
+        }
+    }
+}
